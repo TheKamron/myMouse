908f0c2dd82d2d0bed3b28653f00caa714784dcd
TheKamron <amanovkamronbek778@gmail.com>
2025-10-14 09:03:21 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,76 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const robot = require("robotjs");
+const os = require("os");
+const readline = require("readline");
+const path = require("path");
+const { create  } = require('express-handlebars')
+const exphbs = require('express-handlebars')
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+
+const hbs = exphbs.create({
+  defaultLayout: "main",
+  extname: "hbs",
+  runtimeOptions: {
+    allowProtoPropertiesByDefault: true,
+    allowProtoMethodsByDefault: true,
+  },
+});
+
+app.engine("hbs", hbs.engine);
+app.set("view engine", "hbs");
+app.set("views", "./views");
+
+function getLocalIPv4s() {
+  const ifs = os.networkInterfaces();
+  const ips = [];
+  for (const name of Object.keys(ifs)) {
+    for (const iface of ifs[name]) {
+      if (iface.family === "IPv4" && !iface.internal) {
+        ips.push(iface.address);
+      }
+    }
+  }
+  return ips;
+}
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html")); // <-- HBS o‘rniga HTML
+});
+
+io.on("connection", (socket) => {
+  console.log("✅ Client connected:", socket.id);
+
+  socket.on("move", ({ dx, dy }) => {
+    const mouse = robot.getMousePos();
+    const speed = 3;
+    robot.moveMouse(mouse.x + dx * speed, mouse.y + dy * speed);
+  });
+
+  socket.on("click", (btn) => {
+    robot.mouseClick(btn);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("❌ Client disconnected:", socket.id);
+  });
+});
+
+const PORT = 3000;
+server.listen(PORT, "0.0.0.0", () => {
+  const ips = getLocalIPv4s();
+  console.log("\n🌐 Server running on:");
+  ips.forEach((ip) => console.log(`  👉 http://${ip}:${PORT}`));
+  console.log("\n📱 Telefoningizda shu manzillardan birini oching (bir Wi-Fi’da bo‘ling).");
+  console.log("\nPress ENTER to exit...");
+
+  const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+  rl.on("line", () => process.exit(0));
+});
